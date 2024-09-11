4347da1d85162ed640c52ac6e576f7dad96e1b0d
Silvanos Eric <ericoderito@gmail.com>
2024-09-11 07:45:34 +0300

    feat(song): add song attributes and tracking functionality
    Added several class attributes to `Song` and methods to track the number of songs, genres, and
    artists. Updated the `Song` class to include:
    
    - `count`: total number of songs
    - `genres`: list of genres
    - `artists`: list of artists
    - `genre_count`: count of songs per genre
    - `artist_count`: count of songs per artist
    
    Added corresponding methods:
    - `add_song_to_count`
    - `add_to_genres`
    - `add_to_artists`
    - `add_to_genre_count`
    - `add_to_artist_count`

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,46 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        Song.add_to_artists(artist)
+        Song.add_to_genres(genre)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, new_genre):
+        genres_set = set(cls.genres)
+        genres_set.add(new_genre)
+        cls.genres = list(genres_set)
+
+    @classmethod
+    def add_to_artists(cls, new_artist):
+        artists_set = set(cls.artists)
+        artists_set.add(new_artist)
+        cls.artists = list(artists_set)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre not in cls.genre_count:
+            cls.genre_count[genre] = 1
+        else:
+            cls.genre_count[genre] += 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist not in cls.artist_count:
+            cls.artist_count[artist] = 1
+        else:
+            cls.artist_count[artist] += 1
